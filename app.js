72db0de64d58098eea612e39a59afa64b08f28b6
Ho Nyeong <dev.honing@gmail.com>
2023-12-29 15:36:26 +0900

    app.js 생성하고 index.html 서빙

---
+++ app.js
@@ -0,0 +1,18 @@
+// app.js
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// 루트 경로에 대한 GET 요청 처리
+app.get('/', (req, res) => {
+  const indexPath = path.join(__dirname, 'index.html');
+  res.sendFile(indexPath);
+});
+
+// 서버 시작
+app.listen(port, () => {
+  console.log(`서버 ON: http://localhost:${port}`);
+});
