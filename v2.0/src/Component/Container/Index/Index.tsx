24dfea351106cdd2b4410a6a04e4664a7a75acac
Ho Nyeong <dev.honing@gmail.com>
2023-12-30 21:16:07 +0900

    Index 컴포넌트의 내용을 하드코딩

--- v2.0/src/Component/Container/Index/Index.tsx
+++ v2.0/src/Component/Container/Index/Index.tsx
@@ -5,6 +5,25 @@ import './Index.css'
 const Index = () => {
   return (
     <div id="index">
+      <ul>
+        <li>김우진</li>
+        <li>김현</li>
+        <li>방승희</li>
+        <li>변호녕</li>
+        <li>소사무엘</li>
+        <li>송영준</li>
+        <li>신동현</li>
+        <li>오승민</li>
+        <li>유승민</li>
+        <li>윤준현</li>
+        <li>이민구</li>
+        <li>이유안</li>
+        <li>이채이</li>
+        <li>정영식</li>
+        <li>최성민</li>
+        <li>최은철</li>
+        <li>홍문기</li>
+      </ul>
     </div>
   );
 };
