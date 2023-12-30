e8729d8d996acd5ee3a3da7876fe3e617b5e44c4
Ho Nyeong <dev.honing@gmail.com>
2023-12-30 19:02:08 +0900

    과제 내용에 맞게 사용할 컴포넌트만 적용

--- v2.0/src/App.tsx
+++ v2.0/src/App.tsx
@@ -1,15 +1,11 @@
 // src/App.tsx
 
-import Header from "./Component/Header";
 import Container from "./Component/Container";
-import Footer from "./Component/Footer";
 
 const App = () => {
   return (
     <div>
-      <Header />
       <Container />
-      <Footer />
     </div>
   );
 };
--- v2.0/src/Component/Index.tsx
+++ v2.0/src/Component/Index.tsx
@@ -5,7 +5,6 @@ import './Index.css'
 const Index = () => {
   return (
     <div id="index">
-      <p>#container의 #index의 자식요소로 추가되는지 확인하기</p>
     </div>
   );
 };
