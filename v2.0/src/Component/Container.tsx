8c60261f1e0d4aef0300eb76eed4ab3d11ae9095
Ho Nyeong <dev.honing@gmail.com>
2023-12-30 10:43:02 +0900

    Container 컴포넌트의 자식요소로 Index 컴포넌트 추가
    - Container.tsx에 Index 컴포넌트를 정의
    - Index 컴포넌트의 p 태그에 빨간색 폰트 색상이 적용되는지 확인

--- v2.0/src/Component/Container.tsx
+++ v2.0/src/Component/Container.tsx
@@ -1,11 +1,13 @@
 // src/Component/Container.tsx
 
 import "./Container.css";
+import Index from "./Index";
 
 // 함수형 컴포넌트 Container 생성
 const Container = () => {
   return (
     <div id="container">
+      < Index />
       <div className="content-introduce">
         <h1>변호녕 소개</h1>
         <div className="introduce"></div>
---
+++ v2.0/src/Component/Index.tsx
@@ -0,0 +1,13 @@
+// src/Component/Index.tsx
+
+import './Index.css'
+
+const Index = () => {
+  return (
+    <div id="index">
+      <p>#container의 #index의 자식요소로 추가되는지 확인하기</p>
+    </div>
+  );
+};
+
+export default Index;
